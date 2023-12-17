565bcda1e58ecf7d16b2d5ed8c44c9d73b4d8e14
Ian Covaliuc <ian.covaliuc@gmail.com>
2023-12-17 17:02:15 +0200

    Add the ability to select 2 items

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import './App.css';
+
 import countriesAndCapitalsDB from './database/country-and-capitals-db.json';
 import shuffleCountriesAndCapitals from "./utils/shuffleCountriesAndCapitals.js";
+
+import {useState} from "react";
 
 const countries = Object.keys(countriesAndCapitalsDB);
 const capitals = Object.values(countriesAndCapitalsDB);
 const shuffledCountriesAndCapitals = shuffleCountriesAndCapitals([...countries, ...capitals]);
 
 function App() {
+    const [firstSelection, setFirstSelection] = useState();
+    const [secondSelection, setSecondSelection] = useState();
+
+    function handleClick(index) {
+        const isHasFirstSelection = firstSelection === undefined;
+        const isHasSecondSelection = secondSelection === undefined;
+        const isHasBothSelected = !isHasFirstSelection && !isHasSecondSelection;
+
+        if (isHasBothSelected) {
+            setSecondSelection(undefined);
+            setFirstSelection(undefined);
+        }
+
+        if (!isHasFirstSelection) {
+            setSecondSelection(shuffledCountriesAndCapitals[index]);
+        } else {
+            setFirstSelection(shuffledCountriesAndCapitals[index]);
+        }
+    }
+
     return (
         <div className={'game-wrapper'}>
             {shuffledCountriesAndCapitals.map((countryOrCapital, index) => {
                 return (
                     <div key={index}
-                         className={'country-or-capital'}
+                         className={`country-or-capital`}
+                         onClick={() => handleClick(index)}
                     >
                         {countryOrCapital}
                     </div>
