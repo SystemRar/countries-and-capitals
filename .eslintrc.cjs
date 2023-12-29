79aa7ec6b1de2c104d5308aaba9bf5354b2d9c7b
Ian Covaliuc <ian.covaliuc@gmail.com>
2023-12-29 18:00:14 +0200

    Add Airbnb config and simple-import-sort

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,34 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        "react-app",
+        "react-app/jest"
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+    settings: {react: {version: '18.2'}},
+    plugins: [
+        'react-refresh',
+        "simple-import-sort",
+        "react-hooks"
+    ],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        "simple-import-sort/imports": "warn",
+        "simple-import-sort/exports": "warn",
+        "react-hooks/rules-of-hooks": "warn",
+        "react-hooks/exhaustive-deps": "warn",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-uses-react": "off",
+    },
 }
