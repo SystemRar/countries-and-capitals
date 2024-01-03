98706b3c760884bc6e08e1ebe88e6de7445f1875
Ian Covaliuc <ian.covaliuc@gmail.com>
2024-01-03 18:36:42 +0200

    Change styles to modules

---
+++ src/App.module.css.d.ts
@@ -0,0 +1,13 @@
+declare const country_or_capital: string;
+declare const country_or_capital__incorrectly_selected: string;
+declare const country_or_capital__selected: string;
+declare const end_game: string;
+declare const game_wrapper: string;
+
+export {
+  country_or_capital,
+  country_or_capital__incorrectly_selected,
+  country_or_capital__selected,
+  end_game,
+  game_wrapper,
+};
